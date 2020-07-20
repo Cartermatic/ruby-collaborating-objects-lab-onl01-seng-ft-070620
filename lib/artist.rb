f9eeff13397cfb6b29eee38aa9eedffe4784f6e9
Carter Parrett <cparrett94@hotmail.com>
2020-07-20 23:45:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   end
   
   def self.find_by_name(name)
-    Song.all.find {|song| song.name == self}
+    name.all.find {|song| song.name == self}
   
   def self.find_or_create_by_name
   
