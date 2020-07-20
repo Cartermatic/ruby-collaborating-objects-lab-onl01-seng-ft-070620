679c5aa11fc63266fc00953505da3ff57c33790c
Carter Parrett <cparrett94@hotmail.com>
2020-07-20 23:39:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,14 +29,3 @@ class Artist
 end
 
 
-# describe '#add_song' do
-#     it 'keeps track of an artist\'s songs' do
-#       artist = Artist.new('Michael Jackson')
-#       song_one = Song.new("Rock With You")
-#       song_two = Song.new("Smooth Criminal")
-#       smells_like_teen_spirit = Song.new("Smells Like Teen Spirit")
-#       artist.add_song(song_one)
-#       artist.add_song(song_two)
-#       expect(artist.songs).to eq([song_one, song_two])
-#     end
-#   end
